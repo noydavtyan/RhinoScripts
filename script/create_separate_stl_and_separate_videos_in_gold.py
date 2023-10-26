040f0028911b42f7d87f42ce5efe980d0d21849e
Noy Davtyan <noy.davtyan@exxeta.com>
2023-10-26 22:53:45 +0200

    9th commit

--- script/create_separate_stl_and_separate_videos_in_gold.py
+++ script/create_separate_stl_and_separate_videos_in_gold.py
@@ -41,28 +41,29 @@ def main():
             ########################################
             functions.select_objects_in_layer(str(i))
             rs.Command("_Unisolate")
+            rs.UnselectAllObjects()
+
+            # Get the path of the current script (as a replacement for the .bat file path)
+            base_path = rs.DocumentPath()
+            doc_name = rs.DocumentName()
+            # Command to run the external Python script to add logos to all the images in the Captures folder
+            command = ['C:/Users/noyda/AppData/Local/Programs/Python/Python37/python.exe', 
+                    'G:/Meine Ablage/3D Modelling/#s9hU_All_logos/add_logo_to_images.py',
+                    base_path, doc_name]
+
+            # This will suppress the console window
+            startupinfo = subprocess.STARTUPINFO()
+            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+
+            subprocess.Popen(command, startupinfo=startupinfo)
+
+            sc.doc.Modified = False
+            #rs.Command("_Exit")
 
     
     
 
 if __name__ == '__main__':
     main()
-
-    # Get the path of the current script (as a replacement for the .bat file path)
-    base_path = rs.DocumentPath()
-    doc_name = rs.DocumentName()
-    # Command to run the external Python script to add logos to all the images in the Captures folder
-    command = ['C:/Users/noyda/AppData/Local/Programs/Python/Python37/python.exe', 
-            'G:/Meine Ablage/3D Modelling/#s9hU_All_logos/add_logo_to_images.py',
-            base_path, doc_name]
-
-    # This will suppress the console window
-    startupinfo = subprocess.STARTUPINFO()
-    startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-
-    subprocess.Popen(command, startupinfo=startupinfo)
-
-    sc.doc.Modified = False
-    #rs.Command("_Exit")
 
 
