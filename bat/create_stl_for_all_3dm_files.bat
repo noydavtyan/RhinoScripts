4414f0555e56850f5994ce0226565a95b329ddd6
Noy Davtyan <noy.davtyan@exxeta.com>
2023-10-28 14:10:23 +0200

    Fixed all for all 3dm bats

--- bat/create_stl_for_all_3dm_files.bat
+++ bat/create_stl_for_all_3dm_files.bat
@@ -4,9 +4,20 @@ setlocal
 :: Get the path to the Python script
 set "PYTHON_SCRIPT=C:\Users\noyda\AppData\Roaming\McNeel\Rhinoceros\7.0\scripts\RhinoScripts\scripts\create_stl.py"
 
+:: Set the countdown duration in seconds (e.g., 5 seconds)
+set "COUNTDOWN_DURATION=30"
+
 :: Iterate over each .3dm file in the current directory
 for %%i in (*.3dm) do (
     echo Processing file: %%i
+
+    :: Display the countdown
+    for /l %%s in (%COUNTDOWN_DURATION%,-1,1) do (
+        echo Starting processing in %%s seconds...
+        timeout /t 1 > nul
+    )
+
+    :: Start Rhino and run the Python script
     start "" "C:\Program Files\Rhino 7\System\Rhino.exe" "%%~fi" /runscript="_-RunPythonScript ""%PYTHON_SCRIPT%"""
 )
 
--- scripts/create_stl.py
+++ scripts/create_stl.py
@@ -1,20 +1,20 @@
 import rhinoscriptsyntax as rs
 import functions as functions
+import scriptcontext as sc
 
         
 def main():
     rs.UnselectAllObjects()
     rs.Command("_ZEA")
-    all_layers = rs.LayerNames()
-    for i in range(30):
-        if str(i) in all_layers:
-            functions.export_current_to_stl(i)
+    
+    functions.export_current_to_stl()
     
     
 
 if __name__ == '__main__':
     main()
 
+    sc.doc.Modified = False
     rs.Command("_Exit")
 
 
